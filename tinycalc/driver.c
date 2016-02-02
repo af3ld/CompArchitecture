12a229ab5fd4ea6375fc910c52fb3a3128c168a8
af3ld <alexfeldman@lclark.edu>
2016-02-01 17:52:17 -0800

    Program is taking shape

--- tinycalc/driver.c
+++ tinycalc/driver.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include "tinycalc.h"
-
-
+#include <ctype.h>
+#include <stdlib.h>
 
 
 
 int main(void) {
+	char op; double num;
+	double res = 0.0;
+	int go = 1;
+	tc_memory_t mem; mem.most_recent = 0;
 
-  printf("\nWelcome to TinyCalc!\n\n Enter an operation <+, - , *, /, ^>");
-  printf(" followed by operand\n\n Enter 'q' or 'Q' to quit.\n\n");
-  printf(" Enter 'm' or 'M' followed by location (0-4) to load a previous\n");
-  printf(" result from memory.");
-  printf("\n>");
+	printf("\nWelcome to TinyCalc!\n\n Enter an operation <+, - , *, /, ^>");
+	printf(" followed by operand\n\n Enter 'q' or 'Q' to quit.\n\n");
+	printf(" Enter 'm' or 'M' followed by location (0-4) to load a previous\n");
+	printf(" result from memory.");
 
-  return(0);
+	mem_save(&mem, 0);
+	while (go) {
+		printf("\n>");
+		if (read_command(&op, &num) == 0) {
+			go = 0;
+		} else if (check_command(op)) {
+			if (tolower(op) == 'm') {
+				printf("%lf", mem_read(mem, num));
+			} else {
+				execute_calculation(op, num, &res);
+				printf("%.2lf", res);
+			}
+		}
+	}
 
+	return (0);
 }
 
 
--- tinycalc/tinycalc.c
+++ tinycalc/tinycalc.c
@@ -1,44 +1,51 @@
-#define TC_COMMAND_QUIT 0
-#define TC_COMMAND_OK 1
-#define TC_COMMAND_INVALID 0
 
 #include "tinycalc.h"
 #include <stdio.h>
-
-typedef struct {
-  double vals[TC_MEM_SZ];
-  int most_recent;
-} tc_memory_t;
+#include <ctype.h>
 
 
 int check_command(char op) {
-	if (tolower(op) != 'm') || (tolower(op) != 'q') || (op != '+') || (op != '-') || (op != '*') || (op != '/') || (op != '^') {
-		return TC_COMMAND_INVALID;
-	} else {
+	op = (isalpha(op)) ? tolower(op) : op;
+	switch (op) {
+	case 'm' :
 		return TC_COMMAND_OK;
+	case 'q':
+		return TC_COMMAND_OK;
+	case '+':
+		return TC_COMMAND_OK;
+	case '-':
+		return TC_COMMAND_OK;
+	case '*':
+		return TC_COMMAND_OK;
+	case '/' :
+		return TC_COMMAND_OK;
+	case '^' :
+		return TC_COMMAND_OK;		
+	default:
+		return TC_COMMAND_INVALID;
 	}
 }
 
 int read_command(char *op, double *num) {
-	scanf("%c  %lf", &op, &num);
-	return (tolower(op) == 'q') ? TC_COMMAND_QUIT : TC_COMMAND_OK;
+	scanf("%c  %lf", op, num);
+	return (*op == ('q' | 'Q')) ? TC_COMMAND_QUIT : TC_COMMAND_OK;
 }
 
 void execute_calculation(char f_operator, double f_operand, double *p_result) {
-	if (f_operator == '+'){
-		p_result = f_operand + p_result;
+	if (f_operator == '+') {
+		*p_result += f_operand;
 	} else if (f_operator == '-') {
-		p_result = f_operand - p_result;
+		*p_result -= f_operand;
 	} else if (f_operator == '*') {
-		p_result = f_operand * p_result;
+		*p_result *= f_operand;
 	} else if (f_operator == '/') {
-		p_result = f_operand / p_result;
-	} else if (f_operator == '^'){
+		*p_result = f_operand;
+	} else if (f_operator == '^') {
 		int i; int result = 1;
-		for (i = 0; i < p_result; i++){
-		    result *= base;
+		for (i = 0; i < *p_result; i++) {
+			result *= f_operand;
 		}
-		p_result = result;
+		*p_result = result;
 	}
 }
 
@@ -48,14 +55,14 @@ double mem_read(tc_memory_t mem, int v) {
 
 void mem_save(tc_memory_t *mem, double v) {
 	int i;
-	if (mem.most_recent < TC_MEM_SZ){
-		mem.vals[TC_MEM_SZ - 1 - mem.most_recent] = v;
-		mem.most_recent++;
+	if ((*mem).most_recent < TC_MEM_SZ) {
+		(*mem).vals[TC_MEM_SZ - 1 - (*mem).most_recent] = v;
+		(*mem).most_recent++;
 	} else {
-		for (i = 4; i >= 1; i--){
-			mem.vals[i] = mem.vals[i - 1];
+		for (i = 4; i >= 1; i--) {
+			(*mem).vals[i] = (*mem).vals[i - 1];
 		}
-		mem.vals[0] = v;
+		(*mem).vals[0] = v;
 	}
-	
+
 }
